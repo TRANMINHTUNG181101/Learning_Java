55487950005e08485243ebb7b2de630286496ac8
Tran Minh Tung <tranminhtung18112001@gmail.com>
2023-05-25 14:47:46 +0700

    Chuong trinh quan ly sach Java MySQL

---
+++ src/main/java/com/example/QL_Sach/AppConfig.java
@@ -0,0 +1,50 @@
+//package com.example.QL_Sach;
+//
+//import com.example.QL_Sach.entity.Book;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Configuration
+//public class AppConfig {
+//    @Bean
+//    public List<Book> getBooks() {
+//        List<Book> books = new ArrayList<>();
+//
+//        Book book1 = new Book();
+//        book1.setId(1L);
+//        book1.setTitle("Lap trinh Web Spring MVC");
+//        book1.setAuthor("Anh Nguyen");
+//        book1.setPrice(10.99);
+//        book1.setCategory("Cong nghe thong tin");
+//        books.add(book1);
+//
+//        Book book2 = new Book();
+//        book2.setId(2L);
+//        book2.setTitle("Lap trinh ung dung Spring");
+//        book2.setAuthor("Huy Cuong");
+//        book2.setPrice(12.99);
+//        book2.setCategory("Cong nghe thong tin");
+//        books.add(book2);
+//
+//        Book book3 = new Book();
+//        book3.setId(3L);
+//        book3.setTitle("Lap trinh ung dung Java");
+//        book3.setAuthor("Xuan Nhan");
+//        book3.setPrice(15.99);
+//        book3.setCategory("Cong nghe thong tin");
+//        books.add(book3);
+//
+//        Book book4 = new Book();
+//        book4.setId(4L);
+//        book4.setTitle("Lap trinh ung dung Web Spring MVC");
+//        book4.setAuthor("Dang Khoa");
+//        book4.setPrice(15.99);
+//        book4.setCategory("Cong nghe thong tin");
+//        books.add(book4);
+//
+//        return books;
+//    }
+//}
---
+++ src/main/java/com/example/QL_Sach/QlSachApplication.java
@@ -0,0 +1,13 @@
+package com.example.QL_Sach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlSachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlSachApplication.class, args);
+	}
+
+}
